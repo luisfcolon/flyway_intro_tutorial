fc1e711bb50a4da0970adf15aa934cd3b0491ee0
Luis Colon <luiscolon62@gmail.com>
2019-04-19 19:17:04 -0400

    Update V1__Create_users_table.sql

--- cthulhu/flyway/sql/up/V1__Create_users_table.sql
+++ cthulhu/flyway/sql/up/V1__Create_users_table.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS users (
   id           UUID PRIMARY KEY NOT NULL,
   firstname    TEXT NOT NULL,
   lastname     TEXT NOT NULL,
-  yes          TEXT NOT NULL,
   created_at   TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT NOW(),
   updated_at   TIMESTAMP WITH TIME ZONE
 );
